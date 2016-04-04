63bc9e9a660e2e518c6b59f6ffe07533c88e8c4b
Filippo <fili27182@gmail.com>
2016-04-04 15:12:31 +0200

    correct random funciton, now x,y are effectivly double

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <time.h>
 using namespace std;
 
 /*
@@ -15,19 +16,19 @@ double hypotenuse(double x, double y){
     return sqrt(x * x + y * y);
 }
 double calculate_pi(int iterations) {
-    double ac = 0.0;
+	double ac = 0.0;
     for (int i=0;i < iterations; i++){
-        double x = random() / RAND_MAX;
-        double y = random() / RAND_MAX;
+        double x = ((double)rand()/(double)RAND_MAX);
+        double y = ((double)rand()/(double)RAND_MAX);
         double d = hypotenuse(x,y);
-        if(d < 1.0){
+		if(d < 1.0){
             ac += 1.0;
         }
     }
-    return 4 * ac / iterations;
+    return 4. * ac / iterations;
 }
 int main(int argc, char** argv) {
-    int n = 100;
+    int n=1000;
     double pi = calculate_pi(n);
     cout << "Pi = " << pi << endl;
     return 0;
